9626ca67819a1f7b973eeae77da45d5c2ec77344
Alexander Seryakov <seryakov78116@gmail.com>
2022-11-22 22:54:14 +0300

    Final step for descriptors topic

---
+++ learn2.3(Descriptors)/learn_2.3_step_10.py
@@ -0,0 +1,112 @@
+""" Необходимо написать программу для представления и управления расписанием телевизионного вещания.
+Для этого нужно объявить класс TVProgram, объекты которого создаются командой:
+
+pr = TVProgram(название канала)
+где название канала - это строка с названием телеканала.
+
+В каждом объекте класса TVProgram должен формироваться локальный атрибут:
+
+items - список из телепередач (изначально список пуст).
+
+В самом классе TVProgram должны быть реализованы следующие методы:
+
+add_telecast(self, tl) - добавление новой телепередачи в список items;
+remove_telecast(self, indx) - удаление телепередачи по ее порядковому номеру (атрибуту __id, см. далее).
+
+Каждая телепередача должна описываться классом Telecast, объекты которого создаются командой:
+
+tl = Telecast(порядковый номер, название, длительность)
+где порядковый номер - номер телепередачи в сетке вещания (от 1 и далее); название - наименование телепередачи;
+длительность - время телепередачи (в секундах - целое число).
+
+Соответственно, в каждом объекте класса Telecast должны формироваться локальные приватные атрибуты:
+
+__id - порядковый номер (целое число);
+__name - наименование телепередачи (строка);
+__duration - длительность телепередачи в секундах (целое число).
+
+Для работы с этими приватными атрибутами в классе Telecast должны быть объявлены соответствующие объекты-свойства (property):
+
+uid - для записи и считывания из локального атрибута __id;
+name - для записи и считывания из локального атрибута __name;
+duration - для записи и считывания из локального атрибута __duration.
+
+Пример использования классов (эти строчки в программе писать не нужно):
+
+pr = TVProgram("Первый канал")
+pr.add_telecast(Telecast(1, "Доброе утро", 10000))
+pr.add_telecast(Telecast(2, "Новости", 2000))
+pr.add_telecast(Telecast(3, "Интервью с Балакиревым", 20))
+for t in pr.items:
+    print(f"{t.name}: {t.duration}")"""
+
+
+class TVProgram:
+    def __init__(self, name):
+        self.channel_name = name
+        self.items = []
+
+    def add_telecast(self, tl):
+        self.items.append(tl)
+
+    def remove_telecast(self, indx):
+        for cast in self.items:
+            if cast.uid == indx:
+                self.items.remove(cast)
+
+
+class Telecast:
+    def __init__(self, id, name, duration):
+        self.uid = id
+        self.name = name
+        self.duration = duration
+
+    @property
+    def uid(self):
+        return self.__id
+        
+    @uid.setter
+    def uid(self, uid):
+        self.__id = uid
+
+    @property
+    def name(self):
+        return self.__name
+        
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def duration(self):
+        return self.__duration
+        
+    @duration.setter
+    def duration(self, duration):
+        self.__duration = duration
+        
+    
+assert hasattr(TVProgram, 'add_telecast') and hasattr(TVProgram, 'remove_telecast'), "в классе TVProgram должны быть методы add_telecast и remove_telecast"
+
+pr = TVProgram("Первый канал")
+pr.add_telecast(Telecast(1, "Доброе утро", 10000))
+pr.add_telecast(Telecast(3, "Новости", 2000))
+t = Telecast(2, "Интервью с Балакиревым", 20)
+pr.add_telecast(t)
+
+pr.remove_telecast(3)
+assert len(pr.items) == 2, "неверное число телеперач, возможно, некорректно работает метод remove_telecast"
+assert pr.items[-1] == t, "удалена неверная телепередача (возможно, вы удаляете не по __id, а по порядковому индексу в списке items)"
+
+assert type(Telecast.uid) == property and type(Telecast.name) == property and type(Telecast.duration) == property, "в классе Telecast должны быть объекты-свойства uid, name и duration"
+
+for x in pr.items:
+    assert hasattr(x, 'uid') and hasattr(x, 'name') and hasattr(x, 'duration')
+
+assert pr.items[0].name == "Доброе утро", "объект-свойство name вернуло неверное значение"
+assert pr.items[0].duration == 10000, "объект-свойство duration вернуло неверное значение"
+
+t = Telecast(1, "Доброе утро", 10000)
+t.uid = 2
+t.name = "hello"
+t.duration = 10

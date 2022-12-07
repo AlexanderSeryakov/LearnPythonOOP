facdeaa684e51de5aeca41960abe0b78d0c5664e
Alexander Seryakov <seryakov78116@gmail.com>
2022-12-07 20:04:29 +0300

    Magic methods __add__, __sub__, __mul__, __truediv__

---
+++ Learn_3.4(__add__)/learn_3.4_step_6_(linked_list).py
@@ -0,0 +1,125 @@
+"""Ранее, в одном из подвигов мы с вами создавали односвязный список с объектами класса StackObj
+(когда один объект ссылается на следующий и так далее):
+
+Давайте снова создадим такую структуру данных. Для этого объявим два класса:
+
+Stack - для управления односвязным списком в целом;
+StackObj - для представления отдельных объектов в односвязным списком.
+
+Объекты класса StackObj должны создаваться командой:
+
+obj = StackObj(data)
+где data - строка с некоторыми данными.
+
+Каждый объект класса StackObj должен иметь локальные приватные атрибуты:
+
+__data - ссылка на строку с переданными данными;
+__next - ссылка на следующий объект односвязного списка (если следующего нет, то __next = None).
+
+Объекты класса Stack создаются командой:
+
+st = Stack()
+и каждый из них должен содержать локальный атрибут:
+
+top - ссылка на первый объект односвязного списка (если объектов нет, то top = None).
+
+Также в классе Stack следует объявить следующие методы:
+
+push_back(self, obj) - добавление объекта класса StackObj в конец односвязного списка;
+pop_back(self) - удаление последнего объекта из односвязного списка.
+
+Дополнительно нужно реализовать следующий функционал (в этих операциях копии односвязного списка создавать не нужно):
+
+# добавление нового объекта класса StackObj в конец односвязного списка st
+st = st + obj
+st += obj
+
+# добавление нескольких объектов в конец односвязного списка
+st = st * ['data_1', 'data_2', ..., 'data_N']
+st *= ['data_1', 'data_2', ..., 'data_N']
+В последних двух строчках должны автоматически создаваться N объектов класса StackObj с данными,
+взятыми из списка (каждый элемент списка для очередного добавляемого объекта)."""
+
+
+class StackObj:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        self.__data = data
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, obj):
+        self.__next = obj
+
+
+class Stack:
+    def __init__(self):
+        self.top = self.back = None
+
+    def push_back(self, obj):
+        if not self.top:
+            self.top = self.back = obj
+        self.back.next = obj
+        self.back = obj
+
+    def pop_back(self):
+        pointer = self.top
+        prev_back = ''
+        while pointer.next:
+            prev_back = pointer
+            pointer = pointer.next
+        self.back = prev_back
+        self.back.next = None
+
+    def __add__(self, other):
+        self.push_back(other)
+        return self
+
+    def __iadd__(self, other):  # можно не определять магический метод, т.к согласно ТЗ + и += вернут одно и то же
+        return self + other
+
+    def __mul__(self, other):
+        for obj in other:
+            self.push_back(StackObj(obj))
+        return self
+
+    def __imul__(self, other):  # можно не определять магический метод, т.к согласно ТЗ * и *= вернут одно и то же
+        return self * other
+
+
+assert hasattr(Stack, 'pop_back'), "класс Stack должен иметь метод pop_back"
+
+st = Stack()
+top = StackObj("1")
+st.push_back(top)
+assert st.top == top, "неверное значение атрибута top"
+
+st = st + StackObj("2")
+st = st + StackObj("3")
+obj = StackObj("4")
+st += obj
+
+st = st * ['data_1', 'data_2']
+st *= ['data_3', 'data_4']
+
+d = ["1", "2", "3", "4", 'data_1', 'data_2', 'data_3', 'data_4']
+h = top
+i = 0
+while h:
+    assert h._StackObj__data == d[
+        i], "неверное значение атрибута __data, возможно, некорректно работают операторы + и *"
+    h = h._StackObj__next
+    i += 1
+
+assert i == len(d), "неверное число объектов в стеке"

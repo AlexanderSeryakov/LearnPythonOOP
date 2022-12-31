2eee3827e8eda01d8d72aae273f8b43b6f8bd8e5
Alexander Seryakov <seryakov78116@gmail.com>
2022-12-31 11:29:23 +0300

    Add tests

--- Learn_5.5_context_manager/learn_5.5_step_5.py
+++ Learn_5.5_context_manager/learn_5.5_step_5.py
@@ -68,4 +68,28 @@ class BoxDefender:
             self.__def_cls._things = self.__temp_cls._things
         return False
 
+#  Tests
 
+b = Box('name', 2000)
+assert b._name == 'name' and b._max_weight == 2000, "неверные значения атрибутов объекта класса Box"
+
+b.add_thing(("1", 100))
+b.add_thing(("2", 200))
+
+try:
+    with BoxDefender(b) as bb:
+        bb.add_thing(("3", 1000))
+        bb.add_thing(("4", 1900))
+except ValueError:
+    assert len(b._things) == 2
+
+else:
+    assert False, "не сгенерировалось исключение ValueError"
+
+try:
+    with BoxDefender(b) as bb:
+        bb.add_thing(("3", 100))
+except ValueError:
+    assert False, "возникло исключение ValueError, хотя, его не должно было быть"
+else:
+    assert len(b._things) == 3, "неверное число элементов в списке _things"

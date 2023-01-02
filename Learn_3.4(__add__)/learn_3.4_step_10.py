a3b68cbb427743656fdddcd428d0b75ecaab0e9b
Alexander Seryakov <seryakov78116@gmail.com>
2023-01-02 18:44:14 +0300

    Max Pooling

--- Learn_3.4(__add__)/learn_3.4_step_10.py
+++ Learn_3.4(__add__)/learn_3.4_step_10.py
@@ -4,7 +4,30 @@ class MaxPooling:
         self.size = size
 
     def __call__(self, matrix, *args, **kwargs):
-        for string in matrix:
-            for elem in string:
-                if type(elem) not in (int, float):
-                    raise ValueError("Неверный формат для первого параметра matrix.")
+        rows = len(matrix)
+        cols = len(matrix[0]) if rows > 0 else 0
+
+        if rows == 0:
+            return [[]]
+
+        if not all(map(lambda x: len(x) == cols, matrix)) or not all(map(
+                lambda row: all(map(lambda x: type(x) in (int, float), row)), matrix)):
+            raise ValueError("Неверный формат для первого параметра matrix.")
+
+        h, w = self.size[0], self.size[1]
+        sh, sw = self.step[0], self.step[1]
+
+        rows_range = (rows - h) // sh + 1
+        cols_range = (cols - w) // sw + 1
+
+        res = [[0] * cols_range for _ in range(rows_range)]
+
+        for i in range(rows_range):
+            for j in range(cols_range):
+                cells = (x for r in matrix[i * sh: i * sh + h] for x in r[j * sw: j * sw + w])
+                res[i][j] = max(cells)
+
+        return res
+
+mp = MaxPooling(step=(2, 2), size=(2,2))
+res = mp([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])    # [[6, 8], [9, 7]]
